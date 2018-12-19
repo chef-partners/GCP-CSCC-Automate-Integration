58d119458900623bb82083c36fd7c5e3fbd8358e
Darren Murray <38757423+dmurray-chef@users.noreply.github.com>
2018-12-19 10:52:32 +0000

    GCP Metadata properties (#7)
    * add gcp metadata properties
    
    * Update app.properties.json
    
    * Update properties.py

--- reporter/app/properties.py
+++ reporter/app/properties.py
@@ -1,8 +1,17 @@
 import json
 import os
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+requests.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 class Properties:
-    def __init__(self):
+    def __init__(self, env = None):
+      if (env == 'gcp'):
+        self.buildPropertiesFromGCPMetadata()
+      else:
+        self.buildPropertiesFromFile()
+
+    def buildPropertiesFromFile(self):
       path = f"{os.getcwd()}/app"
       with open(os.path.join(path,'app.properties.json')) as p:
         propertiesFile = json.load(p)
@@ -13,3 +22,17 @@ class Properties:
         self.serviceAccount = propertiesFile["properties"]["serviceAccount"]
         self.organization = propertiesFile["properties"]["organization"]
         self.sourceId = propertiesFile["properties"]["source"]
+
+    def buildPropertiesFromGCPMetadata(self):
+        self.automateUrl = self.getMetadataAttribute("automate-ip")
+        self.scanProfiles = self.getMetadataAttribute("scan-profiles")
+        self.automateApiToken = self.getMetadataAttribute("automate-api-token")
+        self.csccKey = self.getMetadataAttribute("cscc-key")
+        self.serviceAccount = self.getMetadataAttribute("service-account")
+        self.organization = self.getMetadataAttribute("organization-id")
+        self.sourceId = self.getMetadataAttribute("source-id")
+
+    def getMetadataAttribute(self, attributeName):
+      headers = {"Metadata-Flavor": "Google", "content-type": "application/json"}
+      req = requests.get(f"http://metadata/computeMetadata/v1/instance/attributes/{attributeName}", headers=headers)
+      return req.json()
