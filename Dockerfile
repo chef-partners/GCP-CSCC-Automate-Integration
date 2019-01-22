3b6f3d1d50f17701c987478ccae3b503fe9fbbd4
Darren Murray <38757423+dmurray-chef@users.noreply.github.com>
2019-01-22 10:58:52 +0000

    Container (#9)
    * add dockerfile
    
    * fix get gcp metadata
    
    * fix gcp metadata read

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM python:3.7.2-stretch
 RUN echo "deb http://packages.cloud.google.com/apt cloud-sdk-stretch main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
   apt-get update && apt-get install -y google-cloud-sdk
-RUN git clone https://github.com/chef-partners/GCP-CSCC-Automate-Integration.git
-RUN cd GCP-CSCC-Automate-Integration/reporter/ && python -m venv tsenv && \
-. tsenv/bin/activate && pip install -r requirements.txt && python app/app.py gcp 
+RUN git clone https://github.com/chef-partners/GCP-CSCC-Automate-Integration.git && \
+cd GCP-CSCC-Automate-Integration/reporter/ && python -m venv tsenv && \
+. tsenv/bin/activate && pip install -r requirements.txt && python app/app.py gcp
--- reporter/app/properties.py
+++ reporter/app/properties.py
@@ -24,10 +24,12 @@ class Properties:
         self.sourceId = propertiesFile["properties"]["source"]
 
     def buildPropertiesFromGCPMetadata(self):
+        path = f"{os.getcwd()}/app"
         self.automateUrl = self.getMetadataAttribute("automate-ip")
         self.scanProfiles = self.getMetadataAttribute("scan-profiles")
         self.automateApiToken = self.getMetadataAttribute("automate-api-token")
-        self.csccKey = self.getMetadataAttribute("cscc-key")
+        self.outputCsccKey(self.getMetadataAttribute("cscc-key"))
+        self.csccKey = f"{path}/csccKey.json"
         self.serviceAccount = self.getMetadataAttribute("service-account")
         self.organization = self.getMetadataAttribute("organization-id")
         self.sourceId = self.getMetadataAttribute("source-id")
@@ -36,3 +38,7 @@ class Properties:
       headers = {"Metadata-Flavor": "Google", "content-type": "application/json"}
       req = requests.get(f"http://metadata/computeMetadata/v1/instance/attributes/{attributeName}", headers=headers)
       return req.text
+    
+    def outputCsccKey(self, csccKey):
+      with open('csccKey.json') as f:
+        f.write(csccKey)
