0daa7b9beac63e7c2a69788368d20d5384ddeb4d
Darren Murray <dmurray@chef.io>
2019-01-23 11:10:00 +0000

    fix properties.py

--- reporter/app/properties.py
+++ reporter/app/properties.py
@@ -6,9 +6,6 @@ requests.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 class Properties:
     def __init__(self, env = None):
-      if (env == 'gcp'):
-        self.buildPropertiesFromGCPMetadata()
-      else:
         self.buildPropertiesFromFile()
 
     def buildPropertiesFromFile(self):
@@ -18,27 +15,27 @@ class Properties:
         self.automateUrl = propertiesFile["properties"]["automatePublicIp"]
         self.scanProfiles = propertiesFile["properties"]["scanProfiles"]
         self.automateApiToken = propertiesFile["properties"]["automateApiToken"]
-        self.csccKey = propertiesFile["properties"]["cscc"]
+        self.csccKey = f"{path}/csccKey.json"
         self.serviceAccount = propertiesFile["properties"]["serviceAccount"]
         self.organization = propertiesFile["properties"]["organization"]
         self.sourceId = propertiesFile["properties"]["source"]
 
-    def buildPropertiesFromGCPMetadata(self):
+    def __buildPropertiesFromGCPMetadata(self):
         path = f"{os.getcwd()}/app"
-        self.automateUrl = self.getMetadataAttribute("automate-ip")
-        self.scanProfiles = self.getMetadataAttribute("scan-profiles")
-        self.automateApiToken = self.getMetadataAttribute("automate-api-token")
-        self.outputCsccKey(self.getMetadataAttribute("cscc-key"))
+        self.automateUrl = self.__getMetadataAttribute("automate-ip")
+        self.scanProfiles = self.__getMetadataAttribute("scan-profiles")
+        self.automateApiToken = self.__getMetadataAttribute("automate-api-token")
+        self.__outputCsccKey(self.__getMetadataAttribute("cscc-key"))
         self.csccKey = f"{path}/csccKey.json"
-        self.serviceAccount = self.getMetadataAttribute("service-account")
-        self.organization = self.getMetadataAttribute("organization-id")
-        self.sourceId = self.getMetadataAttribute("source-id")
+        self.serviceAccount = self.__getMetadataAttribute("service-account")
+        self.organization = self.__getMetadataAttribute("organization-id")
+        self.sourceId = self.__getMetadataAttribute("source-id")
 
-    def getMetadataAttribute(self, attributeName):
+    def __getMetadataAttribute(self, attributeName):
       headers = {"Metadata-Flavor": "Google", "content-type": "application/json"}
       req = requests.get(f"http://metadata/computeMetadata/v1/instance/attributes/{attributeName}", headers=headers)
       return req.text
     
-    def outputCsccKey(self, csccKey):
+    def __outputCsccKey(self, csccKey):
       with open('csccKey.json') as f:
         f.write(csccKey)
